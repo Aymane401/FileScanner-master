3cbe9af550feea73e5225d96e1a2b1973bb89ff1
Aymane401 <aymane401@gmail.com>
2021-09-29 14:03:07 -0400

    Initial commit

---
+++ FileScanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,113 @@
+﻿using FileScanner.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using FileScanner;
+
+
+namespace FileScanner.ViewModels
+{
+
+    public class MainViewModel : BaseViewModel
+    {
+
+        
+        private string selectedFolder;
+        private ObservableCollection<string> folderItems = new ObservableCollection<string>();
+         
+        public DelegateCommand<string> OpenFolderCommand { get; private set; }
+        public DelegateCommand<string> ScanFolderCommand { get; private set; }
+
+        public  ObservableCollection<string> FolderItems { 
+            get => folderItems;
+            set 
+            { 
+                folderItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public  string SelectedFolder
+        {
+            get => selectedFolder;
+            set
+            {
+                selectedFolder = value;
+                OnPropertyChanged();
+                ScanFolderCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public  MainViewModel()
+        {
+            OpenFolderCommand = new  DelegateCommand<string>(OpenFolder);
+            ScanFolderCommand = new  DelegateCommand<string>(ScanFolder, CanExecuteScanFolder);
+        }
+
+        private bool  CanExecuteScanFolder(string obj)
+        {
+            return !string.IsNullOrEmpty(  SelectedFolder);
+        }
+
+        private async void OpenFolder(string obj)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    SelectedFolder = fbd.SelectedPath;
+                  
+                    
+
+                }
+            }
+        }
+
+        private async void ScanFolder(string dir)
+        {
+
+            FolderItems = new ObservableCollection<string>(  GetDirs(dir));
+          
+            try
+            {
+                foreach (var item in Directory.EnumerateFiles(dir, "*"))
+                {
+                    FolderItems.Add(item);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+
+       IEnumerable<string> GetDirs(string dir)
+        {
+            var options = new EnumerationOptions()
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            };
+            foreach (var d in Directory.EnumerateDirectories(dir, "*", options))
+            {
+                yield return d;
+            }
+        }
+
+        ///TODO : Tester avec un dossier avec beaucoup de fichier
+        ///TODO : Rendre l'application asynchrone
+        ///TODO : Ajouter un try/catch pour les dossiers sans permission
+
+
+    }
+}
---
+++ FileScanner/Views/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using FileScanner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+
+namespace FileScanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BaseViewModel mainviewmodel ;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainviewmodel = new MainViewModel();
+            this.DataContext = (MainViewModel)mainviewmodel;
+        }
+
+
+
+      
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var directories = Directory.GetDirectories(((MainViewModel)mainviewmodel).SelectedFolder);
+
+            // text1.Text = ((MainViewModel)mainviewmodel).FolderItems[0].ToString();
+
+            // ((MainViewModel)mainviewmodel).ScanFolderCommand();
+
+
+
+
+        }
+        System.Drawing.Image newImage = System.Drawing.Image.FromFile(@"logos/File_Icon.png");
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+
+
+        }
+    }
+}
